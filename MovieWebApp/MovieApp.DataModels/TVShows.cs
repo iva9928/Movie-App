fbb3df29b1c2d69538aa3aa941b9bbdcd4d43614
iva9928 <iva.hadgieva9928@gmail.com>
2024-12-15 16:50:41 +0200

    ModelsDone

---
+++ MovieWebApp/MovieApp.DataModels/TVShows.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApp.DataModels
+{
+    public class TVShows
+    {
+        public TVShows() 
+        {
+            this.Id = Guid.NewGuid();
+        }
+       
+        public Guid Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Genre { get; set; } = null!;
+        public DateTime ReleaseDate { get; set; }
+        public string Casts { get; set; } = null!;
+        //minutes
+        public int Duration { get; set; }
+        public string Country { get; set; } = null!;
+        public string Production { get; set; } = null!;
+        public string TVSeriesImageUrl { get; set; } = null!;
+        public string Description { get; set; } = null!;
+    }
+}
