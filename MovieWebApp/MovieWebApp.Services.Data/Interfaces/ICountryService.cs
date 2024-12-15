822b8096ac0d04d4f954fdb4dc4d5073805b2881
iva9928 <iva.hadgieva9928@gmail.com>
2024-12-16 00:01:56 +0200

    CountryService

---
+++ MovieWebApp/MovieWebApp.Services.Data/Interfaces/ICountryService.cs
@@ -0,0 +1,12 @@
+﻿using MovieApp.DataModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MovieWebApp.Services.Data.Interfaces
+{
+    public interface ICountryService
+    {
+        Task<IEnumerable<Country>> GetAllCountriesAsync();
+        Task<Country?> GetCountryDetailsAsync(string id);
+    }
+}
